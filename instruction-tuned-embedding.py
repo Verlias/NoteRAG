341203bb41b9fba3c677ee8c5d0b4ff7b20cb3ba
Logan <loganvoravong@gmail.com>
2026-02-27 00:42:20 -0500

    Implemented Instruction Tuned Embedding Structure

---
+++ instruction-tuned-embedding.py
@@ -0,0 +1,26 @@
+from google import genai
+from google.genai import types
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+
+client = genai.Client(api_key="")
+
+texts = [
+    "What is the meaning of life?",
+    "What is the purpose of chickens",
+    "How do I make a cake",
+]
+
+result = client.models.embed_content(
+    model="gemini-embedding-001",
+    contents=texts,
+    config=types.EmbedContentConfig(task_type="SEMANTIC_SIMILARITY")
+)
+
+df = pd.DataFrame(
+    cosine_similarity([e.values for e in result.embeddings]),
+    index=texts,
+    columns=texts,
+)
+
+print(df)
